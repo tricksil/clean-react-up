f7f698fe9370b6ab17e1de2a202ab32378bed906
Patrick Silva <patrickeeep12@gmail.com>
2023-04-15 07:21:30 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -55,4 +55,13 @@ describe('Login Component', () => {
       email: 'any_email',
     });
   });
+
+  test('Should call Validation with correct password', () => {
+    const { sut, validationSpy } = makeSut();
+    const passwordInput = sut.getByTestId('password') as HTMLInputElement;
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } });
+    expect(validationSpy.input).toEqual({
+      password: 'any_password',
+    });
+  });
 });
