97e972fca4a3e019e04ef4adfac7a35f9fb8eb34
Patrick Silva <patrickeeep12@gmail.com>
2023-04-15 07:30:14 -0300

    refactor: change Validation interface

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -15,9 +15,11 @@ type SutTypes = {
 
 class ValidationSpy implements Validation {
   errorMessage: string;
-  input: object;
-  validate(input: object): string {
-    this.input = input;
+  fieldName: string;
+  fieldValue: string;
+  validate(fieldName: string, fieldValue: string): string {
+    this.fieldName = fieldName;
+    this.fieldValue = fieldValue;
     return this.errorMessage;
   }
 }
@@ -51,17 +53,15 @@ describe('Login Component', () => {
     const { sut, validationSpy } = makeSut();
     const emailInput = sut.getByTestId('email') as HTMLInputElement;
     fireEvent.input(emailInput, { target: { value: 'any_email' } });
-    expect(validationSpy.input).toEqual({
-      email: 'any_email',
-    });
+    expect(validationSpy.fieldName).toBe('email');
+    expect(validationSpy.fieldValue).toBe('any_email');
   });
 
   test('Should call Validation with correct password', () => {
     const { sut, validationSpy } = makeSut();
     const passwordInput = sut.getByTestId('password') as HTMLInputElement;
     fireEvent.input(passwordInput, { target: { value: 'any_password' } });
-    expect(validationSpy.input).toEqual({
-      password: 'any_password',
-    });
+    expect(validationSpy.fieldName).toBe('password');
+    expect(validationSpy.fieldValue).toBe('any_password');
   });
 });
