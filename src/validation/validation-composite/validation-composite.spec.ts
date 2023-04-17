1b4c42ef4584692c8c74a387496b9d7581212a65
Patrick Silva <patrickeeep12@gmail.com>
2023-04-17 07:41:30 -0300

    test: ensure ValidationCmposite return falsy if there is no error

--- src/validation/validation-composite/validation-composite.spec.ts
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -1,15 +1,16 @@
 import { FieldValidationSpy } from '@/validation/test/mock-field-validation';
 import { ValidationComposite } from './vallidation-composite';
+import { faker } from '@faker-js/faker';
 
 type SubTypes = {
   sut: ValidationComposite;
   fieldValidationsSpy: FieldValidationSpy[];
 };
 
-const makeSut = (): SubTypes => {
+const makeSut = (fieldName: string): SubTypes => {
   const fieldValidationsSpy = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName),
   ];
   const sut = new ValidationComposite(fieldValidationsSpy);
   return {
@@ -20,10 +21,19 @@ const makeSut = (): SubTypes => {
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const { sut, fieldValidationsSpy } = makeSut();
-    fieldValidationsSpy[0].error = new Error('first_error_message');
-    fieldValidationsSpy[1].error = new Error('second_error_message');
-    const error = sut.validate('any_field', 'any_value');
-    expect(error).toBe('first_error_message');
+    const fieldName = faker.database.column();
+    const { sut, fieldValidationsSpy } = makeSut(fieldName);
+    const errorMessage = faker.random.words();
+    fieldValidationsSpy[0].error = new Error(errorMessage);
+    fieldValidationsSpy[1].error = new Error(faker.random.words());
+    const error = sut.validate(fieldName, faker.random.word());
+    expect(error).toBe(errorMessage);
+  });
+
+  test('Should return falsy if there is no error', () => {
+    const fieldName = faker.database.column();
+    const { sut } = makeSut(fieldName);
+    const error = sut.validate(fieldName, faker.random.word());
+    expect(error).toBeFalsy();
   });
 });
