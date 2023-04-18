c1a37f2560f21b2af1e243fb166d5eca0cc2b0dd
Patrick Silva <patrickeeep12@gmail.com>
2023-04-18 16:15:30 -0300

    feat: add env var to API URL

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { DefinePlugin } = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -55,5 +56,10 @@ module.exports = {
     react: 'React',
     'react-dom': 'ReactDOM',
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new DefinePlugin({
+      'process.env.API_URL': JSON.stringify('http://localhost:5050/api'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
