2de3161f3bfc00d83b9c6b9aa05d7be1ffd2442e
Patrick Silva <patrickeeep12@gmail.com>
2023-04-20 15:20:33 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpStatusClient returns 404

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -68,4 +68,13 @@ describe('RemoteAtuhentication', () => {
     const promise = sut.add(mockAddAccountParams());
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
+
+  test('Should throw UnexpectedError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound,
+    };
+    const promise = sut.add(mockAddAccountParams());
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
 });
