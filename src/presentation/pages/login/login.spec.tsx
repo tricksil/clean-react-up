2dcb2ad345737a3ee61cce5f13880c075ccad560
Patrick Silva <patrickeeep12@gmail.com>
2023-04-21 06:49:17 -0300

    test: ensure SignUp present error if SaveAccessToken fails

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -158,9 +158,7 @@ describe('Login Component', () => {
   test('Should present error if SaveAccessToken fails', async () => {
     const { sut, saveAccessTokenMock } = makeSut();
     const error = new InvalidCredentialsError();
-    jest
-      .spyOn(saveAccessTokenMock, 'save')
-      .mockReturnValueOnce(Promise.reject(error));
+    jest.spyOn(saveAccessTokenMock, 'save').mockRejectedValueOnce(error);
     await simulateValidSubmit(sut);
     await waitFor(() => {
       Helper.testElementText(sut, 'main-error', error.message);
--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -191,4 +191,15 @@ describe('SignUp Component', () => {
     );
     expect(history.location.pathname).toBe('/');
   });
+
+  test('Should present error if SaveAccessToken fails', async () => {
+    const { sut, saveAccessTokenMock } = makeSut();
+    const error = new EmailInUseError();
+    jest.spyOn(saveAccessTokenMock, 'save').mockRejectedValueOnce(error);
+    await simulateValidSubmit(sut);
+    await waitFor(() => {
+      Helper.testElementText(sut, 'main-error', error.message);
+    });
+    Helper.testChildCount(sut, 'error-wrap', 1);
+  });
 });
