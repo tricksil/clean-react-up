95c159ab0981689df2d658c65549b3b6eb8b6bf4
Patrick Silva <patrickeeep12@gmail.com>
2023-04-21 05:26:10 -0300

    test: ensure SignUp change passwordConfirmation status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -31,7 +31,7 @@ describe('SignUp Component', () => {
     Helper.testStatusForField(sut, 'name', validationError);
     Helper.testStatusForField(sut, 'email', validationError);
     Helper.testStatusForField(sut, 'password', validationError);
-    Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório');
+    Helper.testStatusForField(sut, 'passwordConfirmation', validationError);
   });
 
   test('Should show name error if Validation fails', () => {
@@ -53,5 +53,12 @@ describe('SignUp Component', () => {
     const { sut } = makeSut({ validationError });
     Helper.populateField(sut, 'password');
     Helper.testStatusForField(sut, 'password', validationError);
+  });
+
+  test('Should show passwordConfirmation error if Validation fails', () => {
+    const validationError = faker.random.words();
+    const { sut } = makeSut({ validationError });
+    Helper.populateField(sut, 'passwordConfirmation');
+    Helper.testStatusForField(sut, 'passwordConfirmation', validationError);
   });
 });
