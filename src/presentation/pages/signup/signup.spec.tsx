be26501cace87e07618a84dc7cd65c9f05b123b6
Patrick Silva <patrickeeep12@gmail.com>
2023-04-21 05:31:18 -0300

    test: ensure Login present valid state if name is valid

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -61,4 +61,10 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'passwordConfirmation');
     Helper.testStatusForField(sut, 'passwordConfirmation', validationError);
   });
+
+  test('Should show valid name state if Validation succeeds', () => {
+    const { sut } = makeSut();
+    Helper.populateField(sut, 'name');
+    Helper.testStatusForField(sut, 'name');
+  });
 });
