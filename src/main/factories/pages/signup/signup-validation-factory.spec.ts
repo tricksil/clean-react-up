2ce28258e8090c038f082de78830678dba4f5124
Patrick Silva <patrickeeep12@gmail.com>
2023-04-21 08:36:37 -0300

    test: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,21 @@
+import { ValidationComposite } from '@/validation/validators';
+import { makeSignUpValidation } from './signup-validation-factory';
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder';
+
+describe('SignUpValidationFactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation();
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...Builder.field('name').required().min(5).build(),
+        ...Builder.field('email').required().email().build(),
+        ...Builder.field('password').required().min(5).build(),
+        ...Builder.field('passwordConfirmation')
+          .required()
+          .min(5)
+          .sameAs('password')
+          .build(),
+      ])
+    );
+  });
+});
