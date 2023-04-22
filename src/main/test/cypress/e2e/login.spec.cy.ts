6ba73599a58c0d907d0b7b80917db3c61126a0ba
Patrick Silva <patrickeeep12@gmail.com>
2023-04-22 07:51:37 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/e2e/login.spec.cy.ts
+++ src/main/test/cypress/e2e/login.spec.cy.ts
@@ -1,5 +1,15 @@
 describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('login');
+  });
   it('Should load with correct initial state', () => {
-    cy.visit('/login');
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴');
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴');
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
   });
 });
