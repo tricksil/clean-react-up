dc7cbc7a3ab83b4cb3c97f5770cebb4bbfa38bf4
Patrick Silva <patrickeeep12@gmail.com>
2023-04-22 08:57:13 -0300

    test: ensure Login present error state if is invalid

--- src/main/test/cypress/e2e/login.spec.cy.ts
+++ src/main/test/cypress/e2e/login.spec.cy.ts
@@ -1,13 +1,29 @@
+import { faker } from '@faker-js/faker';
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login');
   });
   it('Should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readOnly');
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('contain.text', '🔴');
+    cy.getByTestId('password').should('have.attr', 'readOnly');
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴');
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+  it('Should present error state if form is invalind', () => {
+    cy.getByTestId('email').focus().type(faker.random.word());
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Valor inválido')
+      .should('contain.text', '🔴');
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3));
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Valor inválido')
       .should('contain.text', '🔴');
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
