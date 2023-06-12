15d201a02587ab56920e49f9761477c514823efe
Patrick Silva <patrickeeep12@gmail.com>
2023-06-12 10:41:48 -0300

    test: ensure SignUp present correct error if server fails

--- src/main/test/cypress/e2e/signup.spec.cy.ts
+++ src/main/test/cypress/e2e/signup.spec.cy.ts
@@ -64,4 +64,13 @@ describe('SignUp', () => {
     FormHelper.testMainError('Esse e-mail já está em uso');
     FormHelper.testUrl('/signup');
   });
+
+  it('Should present UnexpectedError on default error cases', () => {
+    Http.mockUnexpectedError();
+    simulateValidSubmit();
+    FormHelper.testMainError(
+      'Algo de errado aconteceu. Tente novamente em breve.'
+    );
+    FormHelper.testUrl('/signup');
+  });
 });
