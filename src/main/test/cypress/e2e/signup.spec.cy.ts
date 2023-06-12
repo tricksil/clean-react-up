b7361d13ba188d1b1e67c188eb33dbc9a4d5df4d
Patrick Silva <patrickeeep12@gmail.com>
2023-06-12 09:45:41 -0300

    test: ensure SignUp present correct error if email is in use

--- src/main/test/cypress/e2e/signup.spec.cy.ts
+++ src/main/test/cypress/e2e/signup.spec.cy.ts
@@ -1,5 +1,15 @@
 import { faker } from '@faker-js/faker';
 import * as FormHelper from '../support/form-helper';
+import * as Http from '../support/signup-mocks';
+
+const simulateValidSubmit = (): void => {
+  cy.getByTestId('name').focus().type(faker.name.fullName());
+  cy.getByTestId('email').focus().type(faker.internet.email());
+  const password = faker.random.alphaNumeric(7);
+  cy.getByTestId('password').focus().type(password);
+  cy.getByTestId('passwordConfirmation').focus().type(password);
+  cy.getByTestId('submit').click();
+};
 
 describe('SignUp', () => {
   beforeEach(() => {
@@ -46,5 +56,12 @@ describe('SignUp', () => {
     FormHelper.testInputStatus('passwordConfirmation');
     cy.getByTestId('submit').should('not.have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present EmailInUseError on 403', () => {
+    Http.mockEmailInUseError();
+    simulateValidSubmit();
+    FormHelper.testMainError('Esse e-mail já está em uso');
+    FormHelper.testUrl('/signup');
   });
 });
--- src/main/test/cypress/support/http-mocks.ts
+++ src/main/test/cypress/support/http-mocks.ts
@@ -11,6 +11,17 @@ export const mockInvalidCredentialsError = (url: RegExp): void => {
     }
   ).as('request');
 };
+export const mockEmailInUseError = (url: RegExp): void => {
+  cy.intercept(
+    { method: 'POST', url },
+    {
+      statusCode: 403,
+      body: {
+        error: faker.random.words(),
+      },
+    }
+  ).as('request');
+};
 
 export const mockUnexpectedError = (url: RegExp, method: string): void => {
   cy.intercept(
