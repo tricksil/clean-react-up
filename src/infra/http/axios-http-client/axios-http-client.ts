718d1603e9e3e3e844414e473a887791f4fe7a5f
Patrick Silva <patrickeeep12@gmail.com>
2023-06-25 07:06:21 -0300

    refactor: move duplicated code to a helper method

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -16,10 +16,7 @@ export class AxiosHttpClient implements HttpPostClient, HttpGetClient {
       axiosResponse = error.response;
     }
 
-    return {
-      statusCode: axiosResponse.status,
-      body: axiosResponse.data,
-    };
+    return this.adapt(axiosResponse);
   }
 
   async get(params: HttpGetParams): Promise<HttpResponse> {
@@ -29,6 +26,10 @@ export class AxiosHttpClient implements HttpPostClient, HttpGetClient {
     } catch (error) {
       axiosResponse = error.response;
     }
+    return this.adapt(axiosResponse);
+  }
+
+  private adapt(axiosResponse: AxiosResponse): HttpResponse {
     return {
       statusCode: axiosResponse.status,
       body: axiosResponse.data,
