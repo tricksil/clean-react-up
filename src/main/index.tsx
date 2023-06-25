883f405cd6046d0670e7a948ccdf4af9d8a3037d
Patrick Silva <patrickeeep12@gmail.com>
2023-06-25 16:28:33 -0300

    refactor: move Router to Main Layer

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Router } from '@/presentation/components';
+import Router from '@/main/routes/router';
 import '@/presentation/styles/global.scss';
-import { makeLogin } from './factories/pages/login/login-factory';
-import { makeSignUp } from './factories/pages/signup/signup-factory';
 
 const root = ReactDOM.createRoot(document.getElementById('main'));
 
 root.render(
   <React.StrictMode>
-    <Router makeLogin={makeLogin} makeSignUp={makeSignUp} />
+    <Router />
   </React.StrictMode>
 );
--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { makeLogin } from '@/main/factories/pages/login/login-factory';
+import { makeSignUp } from '@/main/factories/pages/signup/signup-factory';
 import { SurveyList } from '@/presentation/pages';
 
-type Factory = {
-  makeLogin: React.FC;
-  makeSignUp: React.FC;
-};
-
-const Router: React.FC<Factory> = (factory: Factory) => {
+const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route key="login" path="login" Component={factory.makeLogin} />
-        <Route key="signup" path="signup" Component={factory.makeSignUp} />
+        <Route key="login" path="login" Component={makeLogin} />
+        <Route key="signup" path="signup" Component={makeSignUp} />
         <Route key="/" path="/" Component={SurveyList} />
       </Routes>
     </BrowserRouter>
