94ffe59f27b71ba5217ebb07fd5c269ea785dd65
Patrick Silva <patrickeeep12@gmail.com>
2023-06-26 15:23:27 -0300

    refactor: move survey list items to components

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Styles from './survey-list-styles.scss';
-import { Header, Footer, Icon, IconName } from '@/presentation/components';
+import { Header, Footer } from '@/presentation/components';
 
 const SurveyList: React.FC = () => {
   return (
@@ -9,18 +9,6 @@ const SurveyList: React.FC = () => {
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>03</span>
-                <span className={Styles.year}>2023</span>
-              </time>
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
           <li></li>
           <li></li>
           <li></li>
