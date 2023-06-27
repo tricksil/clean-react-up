fd3de2b362132645798711da06f175939b8e96eb
Patrick Silva <patrickeeep12@gmail.com>
2023-06-27 17:08:52 -0300

    test: ensure AuthorizeHttpGetClientDecorator merge token to HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -58,4 +58,23 @@ describe('AuthorizeHttpGetClientDecorator', () => {
       'x-access-token': getStorageSpy.value.accessToken,
     });
   });
+
+  test('Should merge headers to HttpGetClient', async () => {
+    const { sut, getStorageSpy, httpGetClientSpy } = makeSut();
+    getStorageSpy.value = mockAccountModel();
+    const field = faker.word.words();
+    const httpRequest: HttpGetParams = {
+      url: faker.internet.url(),
+      headers: {
+        field,
+      },
+    };
+    await sut.get(httpRequest);
+    expect(httpGetClientSpy.url).toBe(httpRequest.url);
+
+    expect(httpGetClientSpy.headers).toEqual({
+      field,
+      'x-access-token': getStorageSpy.value.accessToken,
+    });
+  });
 });
