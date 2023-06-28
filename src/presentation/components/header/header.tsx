20fa2b6719a074bfa887400fbf7485e20252d6a6
Patrick Silva <patrickeeep12@gmail.com>
2023-06-28 07:36:07 -0300

    feat: ensure logout calls setCurrentAccount with undefined

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,8 +1,18 @@
-import React, { memo } from 'react';
 import Styles from './header-styles.scss';
 import { Logo } from '@/presentation/components';
+import { ApiContext } from '@/presentation/contexts';
+import { useNavigate } from 'react-router-dom';
+import React, { memo, useContext } from 'react';
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+  const { setCurrentAccount } = useContext(ApiContext);
+
+  const logout = (event: React.MouseEvent<HTMLAnchorElement>): void => {
+    event.preventDefault();
+    setCurrentAccount(undefined);
+    navigate('/login', { replace: true });
+  };
   return (
     <header className={Styles.headerWrap}>
       <div className={Styles.headerContent}>
@@ -10,7 +20,9 @@ const Header: React.FC = () => {
 
         <div className={Styles.logoutWrap}>
           <span>Patrick</span>
-          <a href="#">Sair</a>
+          <a onClick={logout} data-testid="logout" href="#">
+            Sair
+          </a>
         </div>
       </div>
     </header>
