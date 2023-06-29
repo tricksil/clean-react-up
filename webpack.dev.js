3fbcdfe1c06a5dda0ed504909a2ea20eef67871a
Patrick Silva <patrickeeep12@gmail.com>
2023-06-29 20:43:52 -0300

    chore: add webpack prod and dev configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,22 +1,11 @@
 const path = require('path');
 const { DefinePlugin } = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'development',
-  entry: './src/main/index.tsx',
-  output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'scss'],
-    alias: {
-      '@': path.join(__dirname, 'src'),
-    },
-  },
-  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -43,23 +32,22 @@ module.exports = {
       },
     ],
   },
+  devtool: 'inline-source-map',
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, 'dist'),
     },
     devMiddleware: {
       writeToDisk: true,
     },
     historyApiFallback: true,
   },
-  // externals: {
-  //   react: 'React',
-  //   'react-dom': 'ReactDOM',
-  // },
   plugins: [
     new DefinePlugin({
       'process.env.API_URL': JSON.stringify('http://localhost:5050/api'),
     }),
-    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './template.dev.html',
+    }),
   ],
-};
+});
