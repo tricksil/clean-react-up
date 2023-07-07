9b36482d04fdd3caa3a4caf99fe213bcfed40674
Patrick Silva <patrickeeep12@gmail.com>
2023-07-07 17:47:23 -0300

    feat: add Calendar to SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,5 +1,5 @@
 import Styles from './survey-result-styles.scss';
-import { Footer, Header, Loading } from '@/presentation/components';
+import { Calendar, Footer, Header, Loading } from '@/presentation/components';
 import { Flipped, Flipper } from 'react-flip-toolkit';
 import React from 'react';
 
@@ -8,7 +8,10 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <h2>Qual é seu framework web favorito?</h2>
+        <hgroup>
+          <Calendar date={new Date()} className={Styles.calendarWrap} />
+          <h2>Qual é seu framework web favorito?</h2>
+        </hgroup>
         <Flipper flipKey="answers">
           <ul className={Styles.answerList}>
             <Flipped flipId="ReactJS">
