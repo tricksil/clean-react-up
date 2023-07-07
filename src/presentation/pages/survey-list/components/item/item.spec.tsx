0a9e09b0ffc34934e598b727e9b2f0e482e1325c
Patrick Silva <patrickeeep12@gmail.com>
2023-07-07 17:20:02 -0300

    refactor: move Calendar to its own Component

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -12,21 +12,16 @@ describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
       didAnswer: true,
-      date: new Date('2023-06-26T00:00:00'),
     });
 
     makeSut(survey);
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp);
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
-    expect(screen.getByTestId('day')).toHaveTextContent('26');
-    expect(screen.getByTestId('month')).toHaveTextContent('jun');
-    expect(screen.getByTestId('year')).toHaveTextContent('2023');
   });
 
   test('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
       didAnswer: false,
-      date: new Date('2022-05-03T00:00:00'),
     });
 
     makeSut(survey);
@@ -35,8 +30,5 @@ describe('SurveyItem Component', () => {
       IconName.thumbDown
     );
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
-    expect(screen.getByTestId('day')).toHaveTextContent('03');
-    expect(screen.getByTestId('month')).toHaveTextContent('mai');
-    expect(screen.getByTestId('year')).toHaveTextContent('2022');
   });
 });
