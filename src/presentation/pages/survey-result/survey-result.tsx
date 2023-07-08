1331795c7ae801055f3b66685c3f43754d3bb650
Patrick Silva <patrickeeep12@gmail.com>
2023-07-08 17:32:53 -0300

    feat: add skeleton with shimer on SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -8,22 +8,43 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <hgroup>
-          <Calendar date={new Date()} className={Styles.calendarWrap} />
-          <h2>Qual é seu framework web favorito?</h2>
-        </hgroup>
-        <Flipper flipKey="answers">
-          <ul className={Styles.answerList}>
-            <Flipped flipId="ReactJS">
-              <li className={Styles.active}>
-                <img src="https://ionicframework.com/docs/icons/logo-react-icon.png" />
-                <span className={Styles.answer}>ReactJS</span>
-                <span className={Styles.percent}>50%</span>
-              </li>
-            </Flipped>
-          </ul>
-        </Flipper>
-        <button>Voltar</button>
+        {false && (
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap} />
+              <h2>
+                Qual é seu framework web favorito? Qual é seu framework web
+                favorito?
+              </h2>
+            </hgroup>
+            <Flipper flipKey="answers">
+              <ul className={Styles.answerList}>
+                <Flipped flipId="ReactJS">
+                  <li className={Styles.active}>
+                    <img src="https://ionicframework.com/docs/icons/logo-react-icon.png" />
+                    <span className={Styles.answer}>ReactJS</span>
+                    <span className={Styles.percent}>50%</span>
+                  </li>
+                </Flipped>
+                <Flipped flipId="ReactJS">
+                  <li>
+                    <img src="https://ionicframework.com/docs/icons/logo-react-icon.png" />
+                    <span className={Styles.answer}>ReactJS</span>
+                    <span className={Styles.percent}>50%</span>
+                  </li>
+                </Flipped>
+                <Flipped flipId="ReactJS">
+                  <li>
+                    <img src="https://ionicframework.com/docs/icons/logo-react-icon.png" />
+                    <span className={Styles.answer}>ReactJS</span>
+                    <span className={Styles.percent}>50%</span>
+                  </li>
+                </Flipped>
+              </ul>
+            </Flipper>
+            <button>Voltar</button>
+          </>
+        )}
       </div>
       {false && <Loading />}
       <Footer />
