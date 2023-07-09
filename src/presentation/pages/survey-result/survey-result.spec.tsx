37b415bd9184ede787eeaf08cd9e388999496481
Patrick Silva <patrickeeep12@gmail.com>
2023-07-09 07:23:08 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -5,21 +5,26 @@ import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-const history = createMemoryHistory();
+
+const makeSut = (): void => {
+  const history = createMemoryHistory();
+  render(
+    <ApiContext.Provider
+      value={{
+        setCurrentAccount: jest.fn(),
+        getCurrentAccount: () => mockAccountModel(),
+      }}
+    >
+      <Router location={history.location} navigator={history}>
+        <SurveyResult />
+      </Router>
+    </ApiContext.Provider>
+  );
+};
+
 describe('SurveyResult', () => {
   test('Should present correct inital state', async () => {
-    render(
-      <ApiContext.Provider
-        value={{
-          setCurrentAccount: jest.fn(),
-          getCurrentAccount: () => mockAccountModel(),
-        }}
-      >
-        <Router location={history.location} navigator={history}>
-          <SurveyResult />
-        </Router>
-      </ApiContext.Provider>
-    );
+    makeSut();
     const surveyResult = screen.getByTestId('survey-result');
     expect(surveyResult.childElementCount).toBe(0);
     expect(screen.queryByTestId('error')).not.toBeInTheDocument();
