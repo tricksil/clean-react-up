85d47e60f0bcd1429f7cee8a61e0b25654f6dbd6
Patrick Silva <patrickeeep12@gmail.com>
2023-07-12 10:22:51 -0300

    feat: ensure SurveyResult prevent multiple answer click

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -33,6 +33,7 @@ const SurveyResult: React.FC<Props> = ({
   });
 
   const onAnswer = (answer: string): void => {
+    if (state.isLoading) return;
     setState((old) => ({ ...old, isLoading: true }));
     saveSurveyResult
       .save({ answer })
