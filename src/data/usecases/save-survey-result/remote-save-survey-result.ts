6b1d656b20b1df0be586891759c3e9289c41dfdb
Patrick Silva <patrickeeep12@gmail.com>
2023-07-12 07:09:56 -0300

    feat: ensure RemoteSaveSurveyResult returns SurveyResult on 200

--- src/data/usecases/save-survey-result/remote-save-survey-result.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -15,10 +15,12 @@ export class RemoteSaveSurveyResult implements SaveSurveyResult {
       method: 'put',
       body: params,
     });
-
+    const remoteSurveyResult = httpResponse.body;
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        return null;
+        return Object.assign({}, remoteSurveyResult, {
+          date: new Date(remoteSurveyResult.date),
+        });
       case HttpStatusCode.forbidden:
         throw new AccessDeniedError();
       default:
